7e17442acf5be0069e4e4d123841ced6f8e45665
yxj <564197835@qq.com>
2022-07-07 23:34:02 +0800

    optimize 21 but time increase

--- LeetCode/21-merge-two-sorted-lists/Solution.swift
+++ LeetCode/21-merge-two-sorted-lists/Solution.swift
@@ -3,54 +3,48 @@ class Solution {
         var result: ListNode? = nil
         var resultTail: ListNode? = result
         
+        if list1 == nil {
+            return list2
+        }
+        if list2 == nil {
+            return list1
+        }
+
         var node1 = list1
         var node2 = list2
+
+        if list1!.val > list2!.val {
+            result = ListNode(node2!.val)
+            resultTail = result
+            node2 = node2!.next
+        } else {
+            result = ListNode(node1!.val)
+            resultTail = result
+            node1 = node1!.next
+        }
+
         while node1 != nil, node2 != nil {
             if node1!.val > node2!.val {
                 // 添加node2到resultTail
-                if result == nil {
-                    result = ListNode(node2!.val)
-                    resultTail = result
-                    node2 = node2!.next
-                } else {
-                    resultTail!.next = ListNode(node2!.val)
-                    resultTail = resultTail!.next
-                    node2 = node2!.next
-                }
+                resultTail!.next = ListNode(node2!.val)
+                resultTail = resultTail!.next
+                node2 = node2!.next
             } else {
                 // 添加node1到resultTail
-                if result == nil {
-                    result = ListNode(node1!.val)
-                    resultTail = result
-                    node1 = node1!.next
-                } else {
-                    resultTail!.next = ListNode(node1!.val)
-                    resultTail = resultTail!.next
-                    node1 = node1!.next
-                }
-            }
-        }
-        while node1 != nil {
-            if result == nil {
-                result = ListNode(node1!.val)
-                resultTail = result
-                node1 = node1!.next
-            } else {
                 resultTail!.next = ListNode(node1!.val)
                 resultTail = resultTail!.next
                 node1 = node1!.next
             }
         }
+        while node1 != nil {
+            resultTail!.next = ListNode(node1!.val)
+            resultTail = resultTail!.next
+            node1 = node1!.next
+        }
         while node2 != nil {
-            if result == nil {
-                result = ListNode(node2!.val)
-                resultTail = result
-                node2 = node2!.next
-            } else {
-                resultTail!.next = ListNode(node2!.val)
-                resultTail = resultTail!.next
-                node2 = node2!.next
-            }
+            resultTail!.next = ListNode(node2!.val)
+            resultTail = resultTail!.next
+            node2 = node2!.next
         }
         return result
     }
--- LeetCode/21-merge-two-sorted-lists/main.swift
+++ LeetCode/21-merge-two-sorted-lists/main.swift
@@ -5,6 +5,5 @@ let list2 = ListNode(1, ListNode(3, ListNode(4)))
 let answer = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(4))))))
 let result = solution.mergeTwoLists(list1, list2)
 // assert(result == answer)
-print(result)
 
 print("21 OK")
