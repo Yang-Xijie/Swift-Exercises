d4b98c08d2a5994e9a4250a83d9844a5ee765b93
yxj <564197835@qq.com>
2022-08-10 19:41:03 +0800

    complete 70 with higher efficiency

--- LeetCode/70-climbing-stairs/Solution.swift
+++ LeetCode/70-climbing-stairs/Solution.swift
@@ -1,15 +1,13 @@
 class Solution {
-    var resultDict: [Int: Int] = [1: 1, 2: 2, 3: 3]
+    var resultDict: [Int: Int] = [1: 1, 2: 2]
 
     func climbStairs(_ n: Int) -> Int {
-        if resultDict.keys.contains(n) {
-            return resultDict[n]!
-        } else {
-            for i in 3 ... n {
-                let result_i = climbStairs(i - 1) + climbStairs(i - 2)
-                resultDict[i] = result_i
-            } 
-            return resultDict[n]!
+        if n == 1 || n == 2 { return resultDict[n]! }
+
+        for i in 3 ... n {
+            let result_i = resultDict[i - 1]! + resultDict[i - 2]!
+            resultDict[i] = result_i
         }
+        return resultDict[n]!
     }
 }
